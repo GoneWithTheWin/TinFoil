8ba63a6a7f28745de3f331517408b63aad830f2b
Pardeep Shorian <pshorian@cadent.tv>
2020-12-17 22:37:26 -0800

    more api data and cleanup

--- report_dashboard/javascripts/main.js
+++ report_dashboard/javascripts/main.js
@@ -7,23 +7,27 @@ $(document).ready(function() {
         userId = params.get('user_id');
     }
 
+    var advertizerId = '';
+    if(params.has('advertiser')) {
+      advertizerId = params.get('advertiser');
+    }
+
+    var brandCategoryId = '';
+    if(params.has('brand_category')) {
+      brandCategoryId = params.get('brand_category');
+    }
+
     $(document).on("click", "#mainNavTabs button", function(e) {
       $("#mainNavTabs li.activeTab").removeClass("activeTab");
       $(e.target).parent().addClass("activeTab");
     });
+
     $(document).on("mousedown", "#mainNavTabs button", function(e) {
         $('body').removeClass('keyboardMode');
     });
         $(document).on("keydown", "#mainNavTabs button", function(e) {
         $('body').addClass('keyboardMode');
     });
-
-    var dataSet = [
-        ['First ad', 'Main', 'main event', 'JD', 'JD', 'Retail', 'Nice big ad'],
-        ['Second ad', 'Main', 'main event', 'JD', 'JD', 'Retail', 'Nice big ad'],
-        ['Third ad', 'Main', 'main event', 'JD', 'JD', 'Retail', 'Nice big ad'],
-        ['Fourth ad', 'Main', 'main event', 'JD', 'JD', 'Retail', 'Nice big ad']
-    ];
 
     $('#row3').on( 'init.dt', function ( e, settings ) {
         var api = new $.fn.dataTable.Api( settings );
@@ -41,16 +45,55 @@ $(document).ready(function() {
         }
         
         $('#tableToolbar #filterWrapper').append(filter);
+
+        if(advertizerId) {
+          setTimeout(function() {
+            api.search(advertizerId).draw();
+          });
+        }
     } );
 
     var baseUrl = 'https://ec2-13-56-191-71.us-west-1.compute.amazonaws.com/hackathon/show_report';
-    var url = baseUrl + '?report=ad_event&user_id=' + userId;
+    var url = baseUrl + '?report=ad_event&user_id=' + encodeURIComponent(userId);
 
-    $('#eventMainTable').DataTable( {
-        "ajax": {
-            "url": url,
-            "dataSrc": ""
-        },
+    function processWrongTimeStamp(tm) {
+
+      var split = tm.split(' ');
+      var dP = split[0];
+      var tP = split[1];
+      var year = parseInt(dP.substring(0, 4));
+      var month = parseInt(dP.substring(4,6));
+      var day = parseInt(dP.substring(6));
+      
+      var hour = parseInt(tP.substring(0,2));
+      var mins = parseInt(tP.substring(4,5));
+      var secs = parseInt(tP.substring(7));
+
+      var dm = new Date();
+      dm.setFullYear(year); dm.setMonth(month - 1); dm.setDate(day); dm.setHours(hour); dm.setMinutes(mins); dm.setSeconds(secs);
+
+      return dm.getTime();
+    }
+
+    function padItem(i) {
+      return i < 10 ? '0' + i : i;
+    }
+    function formatColumnDate(tm) {
+      var d = new Date(tm);
+
+      return d.getFullYear() + '/' + padItem(d.getMonth() + 1) + '/' + padItem(d.getDay()) + ' ' + padItem(d.getHours()) + ':' + padItem(d.getMinutes()) + ':' + padItem(d.getSeconds());
+    }
+
+    $.getJSON(url, {}, function(data)  {
+      var updatedData = [];
+      for(var i = 0; i < data.length; i++) {
+        var dt = data[i];
+        var ts = processWrongTimeStamp(dt.time_stamp);
+        dt.time_stamp = ts;
+        updatedData.push( dt );
+      }
+      $('#eventMainTable').DataTable( {
+        "data": updatedData,
         "columns": [
             { "data": "ad_title" },
             { "data": "ad_type" },
@@ -60,20 +103,43 @@ $(document).ready(function() {
             { "data": "brand_category" },
             { "data": "ad_description" },
             { "data": "ip_address" },
-            { "data": "device_type"}
+            { "data": "device_type"},
+            { "data": "time_stamp"}
+        ],
+        "order": [[9, "desc"]],
+        "columnDefs": [
+          {
+            "render": function (data, type, row) {
+              return formatColumnDate(data);
+            },
+            "targets": 9
+          }
         ],
         paging: false
+    });
+    });
+    
+
+    var userInfoUrl = baseUrl + '?report=rpt_user&user_id=' + encodeURIComponent(userId);
+
+    $.getJSON(userInfoUrl, {}, function (data) {
+      if(data && data.length) {
+        var single = data[0];
+        $('#totalAdsValue').text(single.impressions);
+        $('#avgAdsValue').text(single.average_advertizer_impressions);
+      }
     });
 
     Highcharts.setOptions({
         colors: ['#2D61FF', '#00CCED', '#13D666', '#FFC72F', '#FF9700', '#E50000', '#FC2C7D', '#A937FD', '#41515F', '#B3C0CB', '#143CBF', '#00A3BE', '#11BF5B', '#BF215F', '#802ABF']
     });
     
-    var timeSeriesUrl = baseUrl + '?report=rpt_user_hour&user_id=' + userId;
-
+    // time series chart
+    var timeSeriesUrl = baseUrl + '?report=rpt_user_hour&user_id=' + encodeURIComponent(userId);
     $.getJSON(timeSeriesUrl, {}, function(dtaa) {
 
       var timeSeriesData = [];
+      
       for(var i = 0; i < dtaa.length; i++) {
         var single = dtaa[i];
         var hrStr = single.hour_string;
@@ -92,11 +158,25 @@ $(document).ready(function() {
         dtObj.setHours(parseInt(tmParts[0]));
         dtObj.setMinutes(parseInt(tmParts[1]));
 
+        
         timeSeriesData.push([ parseInt(dtObj.getTime() / 1000), imps]);
+        
       };
-      console.log('timeseries data is');
-      console.log(timeSeriesData);
-
+      
+      var daysLable = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var monthsLabel = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      function fomratDate(dt) {
+        var day = dt.getDay();
+        var month = dt.getMonth();
+        var hrs = dt.getHours();
+        hrs = hrs < 10 ? '0' + hrs : hrs;
+        var mins = dt.getMinutes();
+        mins = mins < 10 ? '0' + mins : mins;
+        var secs = dt.getSeconds();
+        secs = secs < 10 ? '0' + secs : secs;
+        return daysLable[day] + ', ' + monthsLabel[month] + ' ' + dt.getDate() + ', ' + hrs +':' + mins + ':' + secs;
+      }
+      
       Highcharts.chart('timeSeriesContainer', {
         chart: {
           zoomType: 'x'
@@ -108,7 +188,20 @@ $(document).ready(function() {
           text: ''
         },
         xAxis: {
-          type: 'datetime'
+          type: 'datetime',
+          labels: {
+            formatter: function(dt) {
+              var dd = dt.value * 1000;
+              var dat = new Date(dd);
+              var hours = dat.getHours();
+              hours = hours < 10 ? '0' + hours : hours;
+              var mins = dat.getMinutes();
+              mins = mins < 10 ? '0' + mins : mins;
+              var secs = dat.getSeconds();
+              secs = secs < 10 ? '0' + secs : secs;
+              return hours +':' + mins +':' + secs;
+            }
+          }
         },
         yAxis: {
           title: {
@@ -144,7 +237,13 @@ $(document).ready(function() {
             threshold: null
           }
         },
-  
+        tooltip: {
+          formatter: function() {
+             var dt = new Date(this.x * 1000);
+              return '<div class="tooltip-top">'+ fomratDate(dt) +'</div><br />'+
+                  '<div class="tooltip-high">Ads Viewed: '+ this.y + '</div>';
+          }
+        },
         series: [{
           type: 'area',
           name: 'Ads Viewed',
@@ -153,14 +252,17 @@ $(document).ready(function() {
   });
 
     });
+      
+
+    var topBrandsUrl = baseUrl + '?report=rpt_user_advertizer&user_id=' + encodeURIComponent(userId);
     
-    
-    
-    //var topBrandsData = [{name:"Apple", y:55},{Name:"Google",y: 42},{name:"Portal by Facebook", y:39},{name:"Nest", y:33},{name:"Autonomous", y:32},{name:"Uplift", y:27},{name:"BevMo", y:22},{name:"Chipotle", y:19},{name:"Total Wine", y:18},{name:"Masterclass", y:18}];
-    
-    var topBrandsData = [["Apple",55],["Google",42],["Portal by Facebook",39],["Nest",33],["Autonomous",32],["Uplift",27],["BevMo",22],["Chipotle",19],["Total Wine",18],["Masterclass",18]]
-    
-    // Build the chart
+    $.getJSON(topBrandsUrl, {}, function (data) {
+
+      var topBrandsData = [];
+      for(var i = 0; i < data.length; i++) {
+        topBrandsData.push([data[i].advertizer, data[i].impressions]);
+      }
+      // Build the chart
     Highcharts.chart('topBrandsContainer', {
       credits: {
         enabled: false
@@ -205,5 +307,6 @@ $(document).ready(function() {
         data: topBrandsData
       }]
     });
-    
+    });
+
   });
